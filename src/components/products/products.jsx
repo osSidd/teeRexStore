b5c2965055f4f962c19cd108b6e24ab72858c48e
Osama <107386980+osSidd@users.noreply.github.com>
2023-11-21 18:12:22 +0530

    added left in stock

--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -9,8 +9,10 @@ export default function Products({clothes, addToCart}){
             clothes.length && clothes.map(item => (
                 <div className="shirt-box" key={item.id}>
                     <img className="shirt-img" src={item.imageURL} alt="cloth" />
+                    <h3>{item.name}</h3>
+                    <span style={{color:'crimson'}}>{item.quantity <=3 && item.quantity ? `only ${item.quantity} left in stock` : null}</span>
                     <div className="price-btn">
-                        <span className="price">{item.price}</span>
+                        <span className="price">Rs {item.price}</span>
                         <button 
                             id={item.id} 
                             onClick={addToCart}
