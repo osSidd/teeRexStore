c4d2064681dcf67828d508f2055c3ace38189dda
Osama <107386980+osSidd@users.noreply.github.com>
2023-12-06 21:04:21 +0530

    tweaked webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -4,29 +4,29 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/index.jsx',
-    mode: 'development',
-    optimization:{
-        runtimeChunk: 'single',
-        splitChunks: {
-            chunks: 'all',
-            maxInitialRequests: Infinity,
-            minSize: 0,
-            cacheGroups: {
-                vendor: {
-                  test: /[\\/]node_modules[\\/](!lodash)/, 
-                  // Here I don't want lodash to be included, you can remove                 (!lodash) to inclue it
-                  name(module) {
+    mode: 'production',
+    // optimization:{
+    //     runtimeChunk: 'single',
+    //     splitChunks: {
+    //         chunks: 'all',
+    //         maxInitialRequests: Infinity,
+    //         minSize: 0,
+    //         cacheGroups: {
+    //             vendor: {
+    //               test: /[\\/]node_modules[\\/](!lodash)/, 
+    //               // Here I don't want lodash to be included, you can remove                 (!lodash) to inclue it
+    //               name(module) {
                   
-        // get the name. E.g. node_modules/packageName/not/this/part.js
-        // or node_modules/packageName
-                  const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-        // npm package names are URL-safe, but some servers don't like @ symbols
-                return `npm.${packageName.replace('@', '')}`;
-                },
-              },
-        },
-    },
-    },
+    //     // get the name. E.g. node_modules/packageName/not/this/part.js
+    //     // or node_modules/packageName
+    //               const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+    //     // npm package names are URL-safe, but some servers don't like @ symbols
+    //             return `npm.${packageName.replace('@', '')}`;
+    //             },
+    //           },
+    //     },
+    // },
+    // },
     module: {
         rules: [
             {
@@ -41,16 +41,16 @@ module.exports = {
             }
         ]
     },
-    externals:{
-        scrp: 'scrp'
-    },
+    // externals:{
+    //     scrp: 'scrp'
+    // },
     resolve: {extensions: ["*", ".jsx", ".js"]},
     output: {
-        chunkFilename: 'scripts/[name].[fullhash:8].bundle.js',
+        // chunkFilename: 'scripts/[name].[fullhash:8].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/',
-        filename: 'scripts/[name].[fullhash:8].bundle.js',
-        clean: true,
+        filename:'bundle.js',
+        // filename: 'scripts/[name].[fullhash:8].bundle.js',
     },
     devServer: {
         static: {
